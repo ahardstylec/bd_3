9b37a9f45eb6bbd54690b23e5b4bbb2609f17ae3
ahardstylec <andreas.collmann@gmail.com>
2015-05-18 13:22:43 +0200

    bla

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -14,12 +14,12 @@ class QuestionTest < ActiveSupport::TestCase
             (1..5).each do |anwsernum|
               antworten["antwort_#{anwsernum}"] = anwsernum% 5 == 0
             end
-            questions<< Question.new(answers: antworten, author_email: "author_email_#{author_nummer}@author_email.de", author_name: "author_#{author_nummer}", question: "frage #{index}")
+            questions<< Question.create(answers: antworten, author_email: "author_email_#{author_nummer}@author_email.de", author_name: "author_#{author_nummer}", question: "frage #{index}")
           end
         end
-        Question.connection.batch do
-          questions.each(&:save)
-        end
+        # Question.connection.batch do
+        #   questions.each(&:save)
+        # end
       end
     end
     Question.connection.schema.truncate_table(:questions)
