e66c05a68557d0c225e07c70cd09e2c2eb3fe141
ahardstylec <andreas.collmann@gmail.com>
2015-05-26 23:07:24 +0200

    bla

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,14 +12,17 @@ class Question
   def self.bulk_insert(insert_multifier)
     puts bench = Benchmark.measure {
        (1..100).each do |author_nummer|
+         questions=[]
+         GC.start
          self.connection.batch do
         (1..10000*insert_multifier).each do |index|
           antworten={}
           (1..5).each do |anwsernum|
             antworten["antwort_#{anwsernum}"] = anwsernum% 5 == 0
           end
-          Question.create(answers: antworten, author_email: "author_email_#{author_nummer}@author_email.de", author_name: "author_#{author_nummer}", question: "frage #{index}")
+          questions<< Question.new(answers: antworten, author_email: "author_email_#{author_nummer}@author_email.de", author_name: "author_#{author_nummer}", question: "frage #{index}")
         end
+        questions.each(&:save)
         end
            end
          }
